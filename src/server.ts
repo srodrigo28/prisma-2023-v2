74b72043fe064b7fc0485c579ec8cd49188ae8c7
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-11-02 08:10:30 -0300

    upared 2

--- src/server.ts
+++ src/server.ts
@@ -7,18 +7,18 @@ const port = 8080
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
 
-// app.use((req, res, next) => {
-// 	// Quais são os métodos que a conexão pode realizar na API
-//     // req.header("Access-Control-Allow-Methods");
-//     // res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-//     //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-//     res.header("Access-Control-Allow-Origin", "*");
-//     app.use(cors());
-//     app.use(router)
-//     next();
-// });
+app.use((req, res, next) => {
+	// Quais são os métodos que a conexão pode realizar na API
+    // req.header("Access-Control-Allow-Methods");
+    // res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+    app.use(cors());
+    app.use(router)
+    next();
+});
 
 app.listen(port, () => {
     console.log('Server is running! porta: ' + port)
