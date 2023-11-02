b4a55ac387c56f9fb9250a5b0e39318e4c3b5f21
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-11-02 13:30:56 -0300

    updated-cors

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,10 @@ const port = 8080
 const app = express()
 
 app.use(express.json())
-// app.use(cors())
-
+app.use(cors())
+app.use(router)
+/*** 
+ * @ Out cors
 app.use((req, res, next) => {
 	// Quais são os métodos que a conexão pode realizar na API
     // req.header("Access-Control-Allow-Methods");
@@ -19,7 +21,7 @@ app.use((req, res, next) => {
     app.use(router)
     next();
 });
-
+*/
 app.listen(port, () => {
     console.log('Server is running! porta: ' + port)
 });
