8d38c254c644eeff24e83bc8fea6a9657881c2da
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-11-04 05:00:20 -0300

    updated cors novamente

--- src/server.ts
+++ src/server.ts
@@ -7,22 +7,22 @@ const port = 8080
 const app = express()
 
 app.use(express.json())
-app.use(cors())
-app.use(router)
+// app.use(cors())
+// app.use(router)
 
 /*** 
- * @ Out cors
+ * @ Out cors */
 app.use((req, res, next) => {
 	// Quais são os métodos que a conexão pode realizar na API
     // req.header("Access-Control-Allow-Methods");
     // res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    // Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
     res.header("Access-Control-Allow-Origin", "*");
     app.use(cors());
     app.use(router)
     next();
 });
-*/
+
 app.listen(port, () => {
     console.log('Server is running! porta: ' + port)
 });
